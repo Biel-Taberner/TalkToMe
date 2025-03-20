030ec4750328116976e457166c71db72a96fb232
Biel-Taberner <gtaberner14592@alumnes.iesmanacor.cat>
2025-03-20 14:16:33 +0100

    Added Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import React from "react";
+import Layout from "./components/layout/Layout.tsx";
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Layout />
       <Routes>
         <Route path="/">
           <Route index element={<Home/>}/>
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Layout() {
+
+    return (
+        <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+        </ul>
+    )
+
+}
