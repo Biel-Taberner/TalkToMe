23a913042ba92dbc236a9b2d33422a872c3ea647
Biel-Taberner <gtaberner14592@alumnes.iesmanacor.cat>
2025-04-01 11:12:52 +0200

    Added new props & anims to FuncButton

--- src/components/button/FuncButton.tsx
+++ src/components/button/FuncButton.tsx
@@ -1,6 +1,7 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useGSAPButtonAnimations } from "../../hooks/gsap-animations/useGSAPAnimations";
 
 interface ButtonProps {
     isOutlined : boolean;
@@ -10,6 +11,7 @@ interface ButtonProps {
     textToSpeak?: string;
     speechConfig?: {};
     isPaused?: PauseProps;
+    isToggable?: ToggableProps;
     callback: (textToSpeak? : string, speechConfig?: {}) => void;
 }
 
@@ -19,14 +21,24 @@ interface PauseProps {
     textForResuming: string,
 }
 
-export default function FuncButton({ color, icon, text, isOutlined, callback, speechConfig, textToSpeak, isPaused } : ButtonProps) {
+interface ToggableProps {
+    toggleRef: boolean,
+    toggableColor: string;
+    toggableText: string;
+    toggableIcon?: IconProp;
+}
+
+export default function FuncButton({ color, icon, text, isOutlined, callback, speechConfig, textToSpeak, isPaused, isToggable } : ButtonProps) {
+    
+    useGSAPButtonAnimations();
+    
     return (
-        <button className={`action-button button ${isOutlined ? 'is-outlined' : ''} ${color}`} onClick={(e) => callback(textToSpeak, speechConfig)}>
+        <button className={`action-button button ${isOutlined ? 'is-outlined' : ''} ${isToggable?.toggleRef ? isToggable?.toggableColor : color}`} onClick={(e) => callback(textToSpeak, speechConfig)}>
             <div className="is-flex is-align-items-center button-icon-text-gap">
-                <FontAwesomeIcon className={isPaused?.pauseRef ? 'iconInButton fa-rotate-90' : 'iconInButton'} icon={ isPaused?.pauseRef ? isPaused.iconForResuming : icon } />
+                <FontAwesomeIcon className={isPaused?.pauseRef ? 'iconInButton fa-rotate-90' : 'iconInButton'} icon={ isPaused?.pauseRef ? isPaused.iconForResuming : isToggable?.toggleRef ? isToggable?.toggableIcon : icon } />
                 <div>
                     {
-                        isPaused?.pauseRef ? isPaused?.textForResuming : text
+                        isPaused?.pauseRef ? isPaused?.textForResuming : isToggable?.toggleRef ? isToggable?.toggableText : text
                     }
                 </div>
             </div>
