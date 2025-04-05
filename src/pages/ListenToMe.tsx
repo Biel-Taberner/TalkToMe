d556aa56a2fffc845a96a16b51c22446831bd9c7
Biel-Taberner <gtaberner14592@alumnes.iesmanacor.cat>
2025-04-05 03:01:57 +0200

    Added title meta tag to pages

--- src/pages/ListenToMe.tsx
+++ src/pages/ListenToMe.tsx
@@ -41,57 +41,60 @@ function Render() {
     useGSAPSectionScrollAnimations();
 
     return (
-        <div className="content mt-6">
-            <section className={`section section-scroll`}>
-                {
-                    !browserSupportsSpeechRecognition &&
-                        <ErrorModal icon={faCircleExclamation} contentI18n="browser_not_support_speech_recognition" />
-                }
-                <div className={!browserSupportsSpeechRecognition ? 'disable-page' : ''}>
-                    <BlockInfo titleI18next="ListenToMe" titleContentClasses="section-1 is-capitalized title is-1 mt-6" descriptionI18next="listenToMe_subtitle" descriptionContentClasses="section-1 subtitle mt-3" />
-
-                    <DropdownContainer 
-                        dropdownTrigger={dropdown}
-                        selectedLanguage={selectedVoice} 
-                        i18nDropdownText={"demo_section_1_dropdown_opt"} 
-                        callback={() => browserSupportsSpeechRecognition && setDropdown(!dropdown)}
-                        languagesToMap={languages}
-                        callbackDropdown={setVoiceToUse}
-                        setterDropdownCallback={setSelectedVoice}
-                        propertyInArray="name"
-                        toggleHelperText={{ toggleRef: true, i18nHelperText: "listenToMe_lang_helper_text" }}
-                    />
-
-                    <div className="mt-6 section-2">
-                        <Subsection numOfSubsections={2} titleI18next={["provisional_transcription", "continuity"]} subsectionDescriptionI18next={["listenToMe_interimResult_description", "listenToMe_continuity_description"]} disableTrigger={!browserSupportsSpeechRecognition} callbacks={[() => setInterimToggler(!interimToggler), () => setContinuousToggler(!continuousToggler)]} />
-                        <Subsection numOfSubsections={1} titleI18next={["voice_commands"]} subsectionDescriptionI18next={["listenToMe_voice_commands_description"]} disableTrigger={!browserSupportsSpeechRecognition} callbacks={[() => setVoiceCommandsToggler(!voiceCommandsToggler)]} />
-                    </div>
-
+        <>
+            <title>ListenToMe | Talk&ListenToMe</title>
+            <div className="content mt-6">
+                <section className={`section section-scroll`}>
                     {
-                        voiceCommandsToggler &&
-                            <BlockInfoList mainTitleI18n="voice_commands_list_title" descriptionI18n="voice_commands_list_subtitle" contentList18n={["voice_commands_command_1", "voice_commands_command_2"]} />
+                        !browserSupportsSpeechRecognition &&
+                            <ErrorModal icon={faCircleExclamation} contentI18n="browser_not_support_speech_recognition" />
                     }
-                    
-                    <div className="mt-6 section-3">
-                        <BlockInfo titleI18next="transcription" titleContentClasses="title" displayDescription={false} />
-                        {
-                            !isMicrophoneAvailable &&
-                                <ErrorModal icon={faCircleExclamation} wantsTopMargin={false} contentI18n="microphone_not_enabled" />
-                        }
-                        <div className="buttons mt-5">
-                            <FuncButton isDisabled={!browserSupportsSpeechRecognition} isToggable={{ toggleRef: listening, toggableColor: "is-danger", toggableTextI18n: "stop", toggableIcon: faMicrophoneSlash }} color="is-link" isOutlined icon={faMicrophone} textI18n="listen" callback={() => { !listening ? startListening(isMicrophoneAvailable, setMicrophoneAvailable, SpeechRecognition, selectedVoice.langCode, continuousToggler, interimToggler) : stopListening(SpeechRecognition) }} />
-                            <FuncButton isDisabled={!browserSupportsSpeechRecognition} color="is-success" isOutlined icon={faTrash} textI18n="clear" callback={resetTranscript} />
-                            <FuncButton isDisabled={!browserSupportsSpeechRecognition} color={copyButtonTrigger ? "is-success" : "is-warning"} isOutlined icon={copyButtonTrigger ? faCheck : faCopy} textI18n={copyButtonTrigger ? "copied" : "copy"} callback={() => copyContentButton(trancriptedContent, setCopyButtonTrigger)} />
+                    <div className={!browserSupportsSpeechRecognition ? 'disable-page' : ''}>
+                        <BlockInfo titleI18next="ListenToMe" titleContentClasses="section-1 is-capitalized title is-1 mt-6" descriptionI18next="listenToMe_subtitle" descriptionContentClasses="section-1 subtitle mt-3" />
+
+                        <DropdownContainer 
+                            dropdownTrigger={dropdown}
+                            selectedLanguage={selectedVoice} 
+                            i18nDropdownText={"demo_section_1_dropdown_opt"} 
+                            callback={() => browserSupportsSpeechRecognition && setDropdown(!dropdown)}
+                            languagesToMap={languages}
+                            callbackDropdown={setVoiceToUse}
+                            setterDropdownCallback={setSelectedVoice}
+                            propertyInArray="name"
+                            toggleHelperText={{ toggleRef: true, i18nHelperText: "listenToMe_lang_helper_text" }}
+                        />
+
+                        <div className="mt-6 section-2">
+                            <Subsection numOfSubsections={2} titleI18next={["provisional_transcription", "continuity"]} subsectionDescriptionI18next={["listenToMe_interimResult_description", "listenToMe_continuity_description"]} disableTrigger={!browserSupportsSpeechRecognition} callbacks={[() => setInterimToggler(!interimToggler), () => setContinuousToggler(!continuousToggler)]} />
+                            <Subsection numOfSubsections={1} titleI18next={["voice_commands"]} subsectionDescriptionI18next={["listenToMe_voice_commands_description"]} disableTrigger={!browserSupportsSpeechRecognition} callbacks={[() => setVoiceCommandsToggler(!voiceCommandsToggler)]} />
                         </div>
+
                         {
-                            interimToggler &&
-                                <Textarea contentToShow={interimTranscript} classes={["textarea", "non-resizable", "section-4"]} numRows={1} isDisabled placeholderI18nText="listenToMe_provisional_transcription_placeholder" />
+                            voiceCommandsToggler &&
+                                <BlockInfoList mainTitleI18n="voice_commands_list_title" descriptionI18n="voice_commands_list_subtitle" contentList18n={["voice_commands_command_1", "voice_commands_command_2"]} />
                         }
-                        <Textarea contentToShow={trancriptedContent} classes={["textarea", "section-4"]} isDisabled placeholderI18nText="listenToMe_transcription_placeholder" />
+                        
+                        <div className="mt-6 section-3">
+                            <BlockInfo titleI18next="transcription" titleContentClasses="title" displayDescription={false} />
+                            {
+                                !isMicrophoneAvailable &&
+                                    <ErrorModal icon={faCircleExclamation} wantsTopMargin={false} contentI18n="microphone_not_enabled" />
+                            }
+                            <div className="buttons mt-5">
+                                <FuncButton isDisabled={!browserSupportsSpeechRecognition} isToggable={{ toggleRef: listening, toggableColor: "is-danger", toggableTextI18n: "stop", toggableIcon: faMicrophoneSlash }} color="is-link" isOutlined icon={faMicrophone} textI18n="listen" callback={() => { !listening ? startListening(isMicrophoneAvailable, setMicrophoneAvailable, SpeechRecognition, selectedVoice.langCode, continuousToggler, interimToggler) : stopListening(SpeechRecognition) }} />
+                                <FuncButton isDisabled={!browserSupportsSpeechRecognition} color="is-success" isOutlined icon={faTrash} textI18n="clear" callback={resetTranscript} />
+                                <FuncButton isDisabled={!browserSupportsSpeechRecognition} color={copyButtonTrigger ? "is-success" : "is-warning"} isOutlined icon={copyButtonTrigger ? faCheck : faCopy} textI18n={copyButtonTrigger ? "copied" : "copy"} callback={() => copyContentButton(trancriptedContent, setCopyButtonTrigger)} />
+                            </div>
+                            {
+                                interimToggler &&
+                                    <Textarea contentToShow={interimTranscript} classes={["textarea", "non-resizable", "section-4"]} numRows={1} isDisabled placeholderI18nText="listenToMe_provisional_transcription_placeholder" />
+                            }
+                            <Textarea contentToShow={trancriptedContent} classes={["textarea", "section-4"]} isDisabled placeholderI18nText="listenToMe_transcription_placeholder" />
+                        </div>
                     </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
+        </>
     )
 }
 
--- src/pages/TalkToMe.tsx
+++ src/pages/TalkToMe.tsx
@@ -35,83 +35,86 @@ const Render = () => {
   useGSAPSectionScrollAnimations();
 
   return (
-    <div className="content mt-6">
-      <section className="section section-scroll">
-        {
-          !speechSynthesisSupport &&
-            <ErrorModal icon={faCircleExclamation} contentI18n="speech_synthesis_not_supported" />
-        }
-        <div className={`${speechSynthesisSupport ? '' : 'disable-page'}`}>
-          <BlockInfo titleI18next="TalkToMe" titleContentClasses={`section-1 title is-1 mt-6`} descriptionI18next="demo_section_1_subtitle" descriptionContentClasses="section-1 subtitle mt-3" />
-        
-          <DropdownContainer 
-            dropdownTrigger={dropdown}
-            selectedLanguage={selectedLanguage} 
-            i18nDropdownText={"demo_section_1_dropdown_opt"} 
-            callback={() => speechSynthesisSupport && setDropdown(!dropdown)}
-            languagesToMap={languages}
-            callbackDropdown={setVoiceToUse}
-            setterDropdownCallback={setSelectedLanguage}
-            propertyInArray="name"
-            toggleHelperText={{ toggleRef: true, i18nHelperText: "demo_section_1_dropdown_helper" }}
-          />
-
-          <div className="mt-6 section-2">
-            <Subsection
-              speechInputs={[
-                { speechInputFieldKey: 'rate', speechInputValue: speechSynthesisConfig.rate, speechInputMinValue: 0.1, speechInputMaxValue: 1 },
-                { speechInputFieldKey: 'pitch', speechInputValue: speechSynthesisConfig.pitch, speechInputMinValue: 0, speechInputMaxValue: 2 }
-              ]} 
-              numOfSubsections={2} 
-              titleI18next={['rate', 'pitch']} 
-              subsectionDescriptionI18next={['talkToMe_page_rate_description', 'talkToMe_page_pitch_description']} 
-              callbacks={[
-                (e) => handleChange('rate', e.target.value, setSpeechSynthesisConfig),
-                (e) => handleChange('pitch', e.target.value, setSpeechSynthesisConfig),
-              ]} 
-              disableTrigger={!speechSynthesisSupport} />
-            <Subsection
-              speechInputs={[
-                { speechInputFieldKey: "volume", speechInputValue: speechSynthesisConfig.volume, speechInputMinValue: 0, speechInputMaxValue: 1 }
-              ]} 
-              numOfSubsections={1} 
-              titleI18next={['volume']} 
-              subsectionDescriptionI18next={['talkToMe_page_volume_description']} 
-              callbacks={[
-                (e) => handleChange('volume', e.target.value, setSpeechSynthesisConfig),
-              ]} 
-              disableTrigger={!speechSynthesisSupport} />
-          </div>
-
-          <div className="section-3 mt-6">
-            <BlockInfo titleI18next={t('voice')} />  
+    <>
+      <title>TalkToMe | Talk&ListenToMe</title>
+      <div className="content mt-6">
+        <section className="section section-scroll">
+          {
+            !speechSynthesisSupport &&
+              <ErrorModal icon={faCircleExclamation} contentI18n="speech_synthesis_not_supported" />
+          }
+          <div className={`${speechSynthesisSupport ? '' : 'disable-page'}`}>
+            <BlockInfo titleI18next="TalkToMe" titleContentClasses={`section-1 title is-1 mt-6`} descriptionI18next="demo_section_1_subtitle" descriptionContentClasses="section-1 subtitle mt-3" />
+          
             <DropdownContainer 
-              dropdownTrigger={dropdownVoices}
-              selectedLanguage={{ name: selectedVoice.voiceURI }}
+              dropdownTrigger={dropdown}
+              selectedLanguage={selectedLanguage} 
               i18nDropdownText={"demo_section_1_dropdown_opt"} 
-              callback={() => speechSynthesisSupport && setDropdownVoices(!dropdownVoices)}
-              languagesToMap={voices}
-              callbackDropdown={setSelectedVoice}
+              callback={() => speechSynthesisSupport && setDropdown(!dropdown)}
+              languagesToMap={languages}
+              callbackDropdown={setVoiceToUse}
+              setterDropdownCallback={setSelectedLanguage}
               propertyInArray="name"
-              toggleHelperText={{ toggleRef: true, i18nHelperText: "demo_section_2_voice_dropdown_helper" }}
-              displayIconInTrigger={false}
-              displayIconInTriggerContent={false}
+              toggleHelperText={{ toggleRef: true, i18nHelperText: "demo_section_1_dropdown_helper" }}
             />
-          </div>
 
-          <div className="section-4 mt-6">
-            <BlockInfo titleI18next="content" />
-            <Textarea isDisabled={!speechSynthesisSupport} classes={["textarea"]} placeholderI18nText="demo_section_3_textarea_placeholder" contentToShow={textToSpeak} callback={(e) => setTextToSpeak(e.target.value)} />
-            <div className="buttons mt-5">
-              <FuncButton isDisabled={!speechSynthesisSupport} color="is-success" isOutlined icon={faPlay} textI18n={ t('play_button_text') } callback={() => handleAudioControl('play', paused, textToSpeak, speechSynthesisConfig, selectedVoice, selectedLanguage.langCode)} />
-              <FuncButton isDisabled={!speechSynthesisSupport} color="is-warning" isOutlined icon={faPause} textI18n={t('pause_button_text')} isPaused={{ pauseRef: paused, textForResuming: t('resume_button_text'), iconForResuming: faEject }} callback={() => { setIsPaused(!paused); handleAudioControl(paused ? 'resume' : 'pause', paused, textToSpeak, speechSynthesisConfig, selectedVoice, selectedLanguage.langCode); }} />
-              <FuncButton isDisabled={!speechSynthesisSupport} color="is-danger" isOutlined icon={faStop} textI18n={ t('stop_button_text') } callback={() => handleAudioControl('stop', paused, textToSpeak, speechSynthesisConfig, selectedVoice, selectedLanguage.langCode)} />
+            <div className="mt-6 section-2">
+              <Subsection
+                speechInputs={[
+                  { speechInputFieldKey: 'rate', speechInputValue: speechSynthesisConfig.rate, speechInputMinValue: 0.1, speechInputMaxValue: 1 },
+                  { speechInputFieldKey: 'pitch', speechInputValue: speechSynthesisConfig.pitch, speechInputMinValue: 0, speechInputMaxValue: 2 }
+                ]} 
+                numOfSubsections={2} 
+                titleI18next={['rate', 'pitch']} 
+                subsectionDescriptionI18next={['talkToMe_page_rate_description', 'talkToMe_page_pitch_description']} 
+                callbacks={[
+                  (e) => handleChange('rate', e.target.value, setSpeechSynthesisConfig),
+                  (e) => handleChange('pitch', e.target.value, setSpeechSynthesisConfig),
+                ]} 
+                disableTrigger={!speechSynthesisSupport} />
+              <Subsection
+                speechInputs={[
+                  { speechInputFieldKey: "volume", speechInputValue: speechSynthesisConfig.volume, speechInputMinValue: 0, speechInputMaxValue: 1 }
+                ]} 
+                numOfSubsections={1} 
+                titleI18next={['volume']} 
+                subsectionDescriptionI18next={['talkToMe_page_volume_description']} 
+                callbacks={[
+                  (e) => handleChange('volume', e.target.value, setSpeechSynthesisConfig),
+                ]} 
+                disableTrigger={!speechSynthesisSupport} />
             </div>
-          </div>
 
-        </div>
-      </section>
-    </div>
+            <div className="section-3 mt-6">
+              <BlockInfo titleI18next={t('voice')} />  
+              <DropdownContainer 
+                dropdownTrigger={dropdownVoices}
+                selectedLanguage={{ name: selectedVoice.voiceURI }}
+                i18nDropdownText={"demo_section_1_dropdown_opt"} 
+                callback={() => speechSynthesisSupport && setDropdownVoices(!dropdownVoices)}
+                languagesToMap={voices}
+                callbackDropdown={setSelectedVoice}
+                propertyInArray="name"
+                toggleHelperText={{ toggleRef: true, i18nHelperText: "demo_section_2_voice_dropdown_helper" }}
+                displayIconInTrigger={false}
+                displayIconInTriggerContent={false}
+              />
+            </div>
+
+            <div className="section-4 mt-6">
+              <BlockInfo titleI18next="content" />
+              <Textarea isDisabled={!speechSynthesisSupport} classes={["textarea"]} placeholderI18nText="demo_section_3_textarea_placeholder" contentToShow={textToSpeak} callback={(e) => setTextToSpeak(e.target.value)} />
+              <div className="buttons mt-5">
+                <FuncButton isDisabled={!speechSynthesisSupport} color="is-success" isOutlined icon={faPlay} textI18n={ t('play_button_text') } callback={() => handleAudioControl('play', paused, textToSpeak, speechSynthesisConfig, selectedVoice, selectedLanguage.langCode)} />
+                <FuncButton isDisabled={!speechSynthesisSupport} color="is-warning" isOutlined icon={faPause} textI18n={t('pause_button_text')} isPaused={{ pauseRef: paused, textForResuming: t('resume_button_text'), iconForResuming: faEject }} callback={() => { setIsPaused(!paused); handleAudioControl(paused ? 'resume' : 'pause', paused, textToSpeak, speechSynthesisConfig, selectedVoice, selectedLanguage.langCode); }} />
+                <FuncButton isDisabled={!speechSynthesisSupport} color="is-danger" isOutlined icon={faStop} textI18n={ t('stop_button_text') } callback={() => handleAudioControl('stop', paused, textToSpeak, speechSynthesisConfig, selectedVoice, selectedLanguage.langCode)} />
+              </div>
+            </div>
+
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
